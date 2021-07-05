301c3890584bfebf43aef6160d278d7e80d9857e
Abdul Fakhri <abf.ca@hotmail.com>
2021-07-05 01:12:04 -0700

    declared the needed SASS variables for styling

--- src/App.js
+++ src/App.js
@@ -8,31 +8,38 @@ and validate the it as well, by using the Formik and Yup
 also using Arrow function here
 */
 const Validation = () => (
-  //Using the Formik component here
+  //Using the Formik component here and inside Formik adding html form
   <Formik>
 
-    <form method="POST">
-      <h1>Sign in </h1>
-      <label htmlFor="email">Email</label>
-      <input name="email" type="text" />
+    <div class="login">
+      <form method="POST">
+        <h1>Sign in </h1>
+        <div className="email">
+          <label htmlFor="email">Email</label>
+          <input name="email" type="text" />
+        </div>
+        <div className="password">
+          <label htmlFor="email">Password</label>
+          <input name="password" type="password" />
+        </div>
+        <div className="checkbox">
+          <label className="label_checkbox">
+            <input type="checkbox" /> Remember me
+          </label>
+        </div>
+        <div className="sign_button">
+          <button type="submit" >
+            Sign In
+          </button>
+        </div>
+        <div className="links">
+          <span ><a href="/password_rest" target="_blank">Forget Your Password?</a></span>
+          <span>Dont have an account?<a href="/signup" target="_blank">Sign up</a></span>
+          <span><a href="/resend_email" target="_blank">Resend email confirmation</a></span>
+        </div>
+      </form>
 
-
-      <label htmlFor="email">Password</label>
-      <input name="password" type="password" />
-
-      <label className="label_checkbox">
-        <input type="checkbox" /> Remember me
-      </label>
-
-      <button type="submit" >
-        Sign In
-      </button>
-      <span ><a href="/password_rest" target="_blank">Forget Your Password?</a></span>
-      <span>Dont have an account? <a href="/signup" target="_blank">Sign up</a></span>
-      <span><a href="/resend_email" target="_blank">Resend email confirmation</a></span>
-    </form>
-
-
+    </div>
 
   </Formik >
 
