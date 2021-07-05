ea47d964b8097ec5f15598009b85d7e0b1b024fb
Abdul Fakhri <abf.ca@hotmail.com>
2021-07-05 10:22:32 -0700

    validationSchema is declared for email and password inputs validation handling

--- src/App.js
+++ src/App.js
@@ -9,46 +9,57 @@ also using Arrow function here
 */
 const Validation = () => (
   //Using the Formik component here and inside Formik adding html form
-  <Formik>
-   {props=>{
-   const {values,touched,errors,isSubmitting,handleChange,handleBlur,handleSubmit
-   }=props;
-   return(
-     <div className="login">
-       <form method="POST">
-         <h1>Sign in </h1>
-         <div className="email">
-           <label htmlFor="email">Email</label>
-           <input name="email" type="text" />
-         </div>
-         <div className="password">
-           <label htmlFor="email">Password</label>
-           <input name="password" type="password" />
-         </div>
-         <div className="checkbox">
-           <label className="label_checkbox">
-             <input type="checkbox" /> Remember me
-           </label>
-         </div>
-         <div className="sign_button">
-           <button type="submit" >
-             Sign In
-           </button>
-         </div>
-         <div className="links">
-           <span ><a href="/password_rest" target="_blank">Forget Your Password?</a></span>
-           <span>Dont have an account?<a href="/signup" target="_blank">Sign up</a></span>
-           <span><a href="/resend_email" target="_blank">Resend email confirmation</a></span>
-         </div>
-       </form>
+  <Formik
 
-     </div>
+    validationSchema={
+      Yup.object().shape({
+        email: Yup.string()
+          .email()
+          .required("Email is Required"),
+        password: Yup.string()
+          .required("Password is Not Provided")
+          .min(8, "Password must should be at least 8 chars")
+          .matches(/(?=.*[0-9])/, "Password must contain a number.")
+      })
+    }
 
-   );
+  >
+    {props => {
+      const { values, touched, errors, isSubmitting, handleChange, handleBlur, handleSubmit } = props;
+      return (
+        <div className="login">
+          <form method="POST">
+            <h1>Sign in </h1>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input name="email" type="text" />
+            </div>
+            <div className="password">
+              <label htmlFor="email">Password</label>
+              <input name="password" type="password" />
+            </div>
+            <div className="checkbox">
+              <label className="label_checkbox">
+                <input type="checkbox" /> Remember me
+              </label>
+            </div>
+            <div className="sign_button">
+              <button type="submit" >
+                Sign In
+              </button>
+            </div>
+            <div className="links">
+              <span ><a href="/password_rest" target="_blank">Forget Your Password?</a></span>
+              <span>Dont have an account?<a href="/signup" target="_blank">Sign up</a></span>
+              <span><a href="/resend_email" target="_blank">Resend email confirmation</a></span>
+            </div>
+          </form>
 
-   }}
-  
+        </div>
 
+      );
+
+    }}
   </Formik >
 
 );
