f32e7ef5d9ff0d5a757aebec087dd5946a872313
Abdul Fakhri <abf.ca@hotmail.com>
2021-07-05 09:57:55 -0700

    react state object initialized

--- src/App.js
+++ src/App.js
@@ -10,36 +10,44 @@ also using Arrow function here
 const Validation = () => (
   //Using the Formik component here and inside Formik adding html form
   <Formik>
+   {props=>{
+   const {values,touched,errors,isSubmitting,handleChange,handleBlur,handleSubmit
+   }=props;
+   return(
+     <div className="login">
+       <form method="POST">
+         <h1>Sign in </h1>
+         <div className="email">
+           <label htmlFor="email">Email</label>
+           <input name="email" type="text" />
+         </div>
+         <div className="password">
+           <label htmlFor="email">Password</label>
+           <input name="password" type="password" />
+         </div>
+         <div className="checkbox">
+           <label className="label_checkbox">
+             <input type="checkbox" /> Remember me
+           </label>
+         </div>
+         <div className="sign_button">
+           <button type="submit" >
+             Sign In
+           </button>
+         </div>
+         <div className="links">
+           <span ><a href="/password_rest" target="_blank">Forget Your Password?</a></span>
+           <span>Dont have an account?<a href="/signup" target="_blank">Sign up</a></span>
+           <span><a href="/resend_email" target="_blank">Resend email confirmation</a></span>
+         </div>
+       </form>
 
-    <div class="login">
-      <form method="POST">
-        <h1>Sign in </h1>
-        <div className="email">
-          <label htmlFor="email">Email</label>
-          <input name="email" type="text" />
-        </div>
-        <div className="password">
-          <label htmlFor="email">Password</label>
-          <input name="password" type="password" />
-        </div>
-        <div className="checkbox">
-          <label className="label_checkbox">
-            <input type="checkbox" /> Remember me
-          </label>
-        </div>
-        <div className="sign_button">
-          <button type="submit" >
-            Sign In
-          </button>
-        </div>
-        <div className="links">
-          <span ><a href="/password_rest" target="_blank">Forget Your Password?</a></span>
-          <span>Dont have an account?<a href="/signup" target="_blank">Sign up</a></span>
-          <span><a href="/resend_email" target="_blank">Resend email confirmation</a></span>
-        </div>
-      </form>
+     </div>
 
-    </div>
+   );
+
+   }}
+  
 
   </Formik >
 
