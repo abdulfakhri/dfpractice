991cdcea61ef4e6ea0b57d1a9aa8f680b22dd2cf
Abdul Fakhri <abf.ca@hotmail.com>
2021-07-05 00:39:59 -0700

    SASS files created, fonts made Roboto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik } from "formik";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/*
+Validation function and component is 
+intiated here that consist the form 
+and validate the it as well, by using the Formik and Yup
+also using Arrow function here
+*/
+const Validation = () => (
+  //Using the Formik component here
+  <Formik>
 
-export default App;
+    <form method="POST">
+      <h1>Sign in </h1>
+      <label htmlFor="email">Email</label>
+      <input name="email" type="text" />
+
+
+      <label htmlFor="email">Password</label>
+      <input name="password" type="password" />
+
+      <label className="label_checkbox">
+        <input type="checkbox" /> Remember me
+      </label>
+
+      <button type="submit" >
+        Sign In
+      </button>
+      <span ><a href="/password_rest" target="_blank">Forget Your Password?</a></span>
+      <span>Dont have an account? <a href="/signup" target="_blank">Sign up</a></span>
+      <span><a href="/resend_email" target="_blank">Resend email confirmation</a></span>
+    </form>
+
+
+
+  </Formik >
+
+);
+//we need to export this App function to make it accessible
+export default Validation;
